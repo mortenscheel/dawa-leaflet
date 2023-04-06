692a30333f9cbd4f1e4dc17fa531b504ddefc956
Morten Scheel <morten@mortenscheel.com>
2023-04-06 19:17:14 +0200

    Simplify

--- src/main.js
+++ src/main.js
@@ -1,16 +1,12 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
 
 import App from './App.vue'
-import router from './router'
 
 import '@/assets/styles.scss'
 
 const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
 app.use(PrimeVue)
 
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dawa-vue/',
+  base: '/dawa-leaflet/',
   plugins: [vue()],
   resolve: {
     alias: {
